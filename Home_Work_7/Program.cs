2b1dec49cc90efa7fdc013c8fe262c1affc56c5d
LolyaDi <you@example.com>
2019-03-23 12:41:43 +0600

    Седьмая домашняя работа

---
+++ Home_Work_7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyClassLibrary.WorldOfTanks;
+
+namespace Home_Work_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int COUNT_TANK = 5;
+            const int MAXIMUM_LEVEL = 101;
+            const int MINIMUM_LEVEL = 0;
+
+            int levelAmmunitionLoadTank;
+            int levelArmorTank;
+            int levelManeuverabilityTank;
+
+            Random random = new Random();
+            Tank[] firstTank = new Tank[COUNT_TANK];
+            Tank[] secondTank = new Tank[COUNT_TANK];
+            Tank[] checkTank = new Tank[COUNT_TANK];
+
+            for (int i = 0; i < firstTank.Length; i++)
+            {
+                levelAmmunitionLoadTank = random.Next(MINIMUM_LEVEL, MAXIMUM_LEVEL);
+
+                levelArmorTank = random.Next(MINIMUM_LEVEL, MAXIMUM_LEVEL);
+
+                levelManeuverabilityTank = random.Next(MINIMUM_LEVEL, MAXIMUM_LEVEL);
+
+                firstTank[i] = new Tank("T-34", levelAmmunitionLoadTank, levelArmorTank, levelManeuverabilityTank);
+            }
+
+
+            for (int i = 0; i < secondTank.Length; i++)
+            {
+                levelAmmunitionLoadTank = random.Next(MINIMUM_LEVEL, MAXIMUM_LEVEL);
+
+                levelArmorTank = random.Next(MINIMUM_LEVEL, MAXIMUM_LEVEL);
+
+                levelManeuverabilityTank = random.Next(MINIMUM_LEVEL, MAXIMUM_LEVEL);
+
+                secondTank[i] = new Tank("Pantera", levelAmmunitionLoadTank, levelArmorTank, levelManeuverabilityTank);
+            }
+
+
+            for (int i = 0; i < checkTank.Length; i++)
+            {
+                checkTank[i] = new Tank();
+
+                secondTank[i].СharacteristicsTank();
+                Console.WriteLine("--------------------------------------");
+
+                firstTank[i].СharacteristicsTank();
+                checkTank[i] = firstTank[i] ^ secondTank[i];
+
+                Console.WriteLine();
+                Console.WriteLine("--------------------------------------");
+                checkTank[i].WinnerTank();
+                Console.WriteLine("--------------------------------------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyClassLibrary/WorldOfTanks/Tank.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyClassLibrary.WorldOfTanks
+{
+    public class Tank
+    {
+        private string TankName { get; set; }
+
+        private int LevelAmmunitionLoadTank { get; set; }
+
+        private int LevelArmorTank { get; set; }
+
+        private int LevelManeuverabilityTank { get; set; }
+
+        public Tank()
+        {
+
+        }
+
+        public Tank(string nameTank, int levelAmmunitionLoadTank, int levelArmorTank, int levelManeuverabilityTank)
+        {
+            TankName = nameTank;
+            LevelAmmunitionLoadTank = levelAmmunitionLoadTank;
+            LevelArmorTank = levelArmorTank;
+            LevelManeuverabilityTank = levelManeuverabilityTank;
+        }
+
+        public void СharacteristicsTank()
+        {
+            Console.WriteLine($"Название танка: \"{TankName}\"");
+            Console.WriteLine($"Уровень боекомлекта -  {LevelAmmunitionLoadTank} %");
+            Console.WriteLine($"Уровень брони - {LevelArmorTank} %");
+            Console.WriteLine($"Уровень маневрености - {LevelManeuverabilityTank} %");
+        }
+
+        public static Tank operator ^(Tank firstTank, Tank secondTank)
+        {
+            if (firstTank.LevelAmmunitionLoadTank > secondTank.LevelAmmunitionLoadTank && firstTank.LevelArmorTank > secondTank.LevelArmorTank)
+            {
+                return firstTank;
+            }
+
+            if (firstTank.LevelAmmunitionLoadTank > secondTank.LevelAmmunitionLoadTank && firstTank.LevelManeuverabilityTank > secondTank.LevelManeuverabilityTank)
+            {
+                return firstTank;
+            }
+
+            if (firstTank.LevelArmorTank > secondTank.LevelArmorTank && firstTank.LevelManeuverabilityTank > secondTank.LevelManeuverabilityTank)
+            {
+                return firstTank;
+            }
+
+            else
+            {
+                return secondTank;
+            }
+        }
+
+        public void WinnerTank()
+        {
+            Console.WriteLine($"Победил в сражении танк: {TankName}");
+        }
+    }
+}
